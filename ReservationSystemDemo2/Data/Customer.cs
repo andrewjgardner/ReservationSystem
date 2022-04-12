84b38da3ee3f88ea952b466bc0571db024e2b6f9
andrewjgardner <knowingandy@gmail.com>
2022-04-12 17:03:03 +1000

    Update Person and Customer

--- ReservationSystemDemo2/Data/Customer.cs
+++ ReservationSystemDemo2/Data/Customer.cs
@@ -3,6 +3,8 @@
     public class Customer : Person
     {
         public int Id { get; set; }
+        public string Email { get; set; }
+
 
         public List<Reservation> Reservations = new List<Reservation>();
     }
