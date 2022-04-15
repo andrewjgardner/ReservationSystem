8bf428ff714c846254757aecc2cb37dbd41fd93e
andrewjgardner <knowingandy@gmail.com>
2022-04-15 13:33:07 +1000

    ReservationController with basic Sittings actions

---
+++ ReservationSystem/Models/Reservation/SittingsVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationSystem.Models.Reservation
+{
+    public class SittingsVM
+    {
+        public int SittingID { get; set; }
+        public string? Date { get; set; }
+
+        [Display(Name = "Start time")]
+        public string? StartTime { get; set; }
+
+        [Display(Name = "End time")]
+        public string? EndTime { get; set; }
+
+        [Display(Name = "Name")]
+        public string? Title { get; set; }
+
+    }
+}
