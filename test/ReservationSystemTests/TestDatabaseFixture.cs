70e00395a1094114f51ad5f2e3842510347b4674
andrewjgardner <knowingandy@gmail.com>
2022-04-17 11:06:39 +1000

    Test database fixture for localdb

---
+++ test/ReservationSystemTests/TestDatabaseFixture.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystemTests
+{
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=ReservationSystemTests;Trusted_Connection=True";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public ApplicationDbContext CreateContext()
+            => new ApplicationDbContext(
+                new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlServer(ConnectionString)
+                    .Options);
+
+    }
+}
