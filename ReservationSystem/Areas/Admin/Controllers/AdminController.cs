fe5664af33904ce8c3db3a1686afe62659cdb7c0
conor-oneill-2 <conoroneill71@gmail.com>
2022-04-26 10:29:50 +1000

    Admin Controller Added

---
+++ ReservationSystem/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationSystem.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public AdminController ()
+        {
+
+        }
+
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ReservationSystem/Program.cs
+++ ReservationSystem/Program.cs
@@ -37,8 +37,15 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+  name: "Admin",
+  pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+);
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 app.MapRazorPages();
 
 app.Run();
