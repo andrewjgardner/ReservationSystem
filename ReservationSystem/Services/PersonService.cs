7bc329b2f9492de518f7c93f42b78fcaa2458ddb
deadene <knowingandy@gmail.com>
2022-04-28 15:19:57 +1000

    Create Person Service

---
+++ ReservationSystem/Services/PersonService.cs
@@ -0,0 +1,20 @@
+﻿using ReservationSystem.Data;
+
+namespace ReservationSystem.Services
+{
+    public class PersonService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Person> FindOrCreatePersonAsync(string email, string phonenumber, string firstname, string lastname)
+        {
+            return;
+        }
+        
+    }
+}
