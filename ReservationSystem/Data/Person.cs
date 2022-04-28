831a57db0a37f084df42760fe1a847317aa836be
deadene <knowingandy@gmail.com>
2022-04-28 10:19:05 +1000

    UserId Change

--- ReservationSystem/Data/Person.cs
+++ ReservationSystem/Data/Person.cs
@@ -1,4 +1,6 @@
-﻿namespace ReservationSystem.Data
+﻿using ReservationSystem.Data.Identity;
+
+namespace ReservationSystem.Data
 {
     public class Person
     {
@@ -7,7 +9,8 @@
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
 
-        public int? UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public int? ApplicationUserId { get; set; }
 
         public Restaurant Restaurant { get; set; }
         public int RestaurantId { get; set; }
