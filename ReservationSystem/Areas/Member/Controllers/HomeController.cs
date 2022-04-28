34d69002b75cbed5886133fda4845d78e308a653
Brendan Chappell <brendanauri@gmail.com>
2022-04-28 12:53:36 +1000

    Set up Member area

---
+++ ReservationSystem/Areas/Member/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Areas.Admin.Models;
+using ReservationSystem.Data;
+using ReservationSystem.Models.Reservation;
+
+namespace ReservationSystem.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public HomeController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public async Task<IActionResult> Reservations()
+        //{
+        //    return View();
+        //}
+    }
+}
