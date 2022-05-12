1bc96bc9da52dad5b3002ba31666f275cd38e34e
conor-oneill-2 <conoroneill71@gmail.com>
2022-05-12 16:25:23 +1000

    Switched Create Sitting Forms to asp tag helpers + bugfixing

---
+++ ReservationSystem/wwwroot/js/admin.sitting.create.js
@@ -0,0 +1,23 @@
+﻿$(() => {
+
+	var slider = $('#numberToSchedule');
+	var output = $('numberToScheduleText');
+	output.value = slider.value;
+
+	slider.oninput = function () {
+		output.value = this.value;
+	}
+
+	$('#cb-show-recurring').change((e) => {
+
+		if (e.currentTarget.checked) {
+			$('#recurring-form-container').show();
+		}
+		else {
+			$('#recurring-form-container').hide();
+
+		}
+	}); 
+
+
+});
