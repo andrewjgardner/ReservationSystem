ee179e98f58c81aa8affd0a9b431521e795f867e
conor-oneill-2 <conoroneill71@gmail.com>
2022-05-17 13:54:08 +1000

    Select Days to Recur on for Weekly Recur

--- ReservationSystem/wwwroot/js/admin.sitting.create.js
+++ ReservationSystem/wwwroot/js/admin.sitting.create.js
@@ -17,7 +17,54 @@
 			$('#recurring-form-container').hide();
 
 		}
-	}); 
+	});
 
+	$('#set-recurring-type').change((e) => {
+
+		let recurringtype = $('#set-recurring-type').val();
+
+		if (recurringtype==="Weekly") {
+			$('#weekly-days-select').show();
+		}
+		else {
+			$('#weekly-days-select').hide();
+		}
+	});
+
+	$('#start-time').change((e) => {
+		let startday = new Date($('#start-time').val()).getDay();
+		for (i = 0; i < 7; i++) {
+			$('#recur-day-' + i).attr("disabled", false);
+		}
+		$('#recur-checkbox-day-' + startday).prop("checked", true);
+		$('#recur-hidden-day-' + startday).val(true);
+		$('#recur-checkbox-day-' + startday).attr("disabled", true);
+	});
+
+	for (i = 0; i < 7; i++) {
+		$('#recur-checkbox-day-' + i).change((e) => {
+			$('#recur-hidden-day-' + i).toggle;
+		});
+    }
+
+	function calculateEndDate(date, recurringType, sliderval) {
+		if (recurringType == "Daily") {
+			const addDays = sliderval;
+		}
+		else if (recurringType == "Weekly") {
+			const addD
+
+        }
+	}
+
+	function disableRecurringSittingDay() {
+		let startday = new Date($('#start-time').val()).getDay();
+		for (i = 0; i < 7; i++) {
+			$('#recur-day-' + i).attr("disabled", false);
+		}
+		$('#recur-checkbox-day-' + startday).prop("checked", true);
+		$('#recur-hidden-day-' + startday).val(true);
+		$('#recur-checkbox-day-' + startday).attr("disabled", true);
+    }
 
 });
