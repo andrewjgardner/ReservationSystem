0306a741e524eb02d7ef397caaf5d71ec11c1791
conor-oneill-2 <conoroneill71@gmail.com>
2022-05-19 11:47:14 +1000

    Wants me to commit the changes from remote for some reason.

---
+++ ReservationSystem/Migrations/20220510022739_Update.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationSystem.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "77bd9ac8-87da-4499-8b6f-ad1557c590c3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "452f9cf3-7ebc-417f-a122-bcdbd02954ef");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "afc43a31-b4d8-4d94-bfb3-43c276eb1310");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "3f5f03dd-0d71-4595-974d-270a803e29c9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "d6ccca7f-0ff0-423c-8948-fc5ef53e06fe");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "b7d42bec-b0d1-4e4c-9674-468d33463462");
+        }
+    }
+}
