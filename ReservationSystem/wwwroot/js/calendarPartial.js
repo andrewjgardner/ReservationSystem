586da8bc1844a3b716d87f0acb6d997e4de16a77
damien.antonietti1 <damien.antonietti1@PE00A001004DE12>
2022-05-24 16:20:11 +1000

    adding styling for calendar, also added some testing stuff cuse i forgot to make new branch lol

--- ReservationSystem/wwwroot/js/calendarPartial.js
+++ ReservationSystem/wwwroot/js/calendarPartial.js
@@ -26,17 +26,20 @@ async function GetSittings(date) {
     if (s.length == 0) {
         $("#sittings-partial").html("");
     }
+    let i = 0;
     for (let s of getSittingsOnDay(date,sittings)) {
-        
+        i++;
         let interval = new Date(s.startTime);
         let endTime = new Date(s.endTime)
 
         let el = $($('#accordian-template').html());
         el.find('#sitting-times-header-button').html(`${s.title} ${s.startTime} ${s.endTime}`);
-     
+        el.find('#sitting-times-header-button').attr('data-bs-target', `#collpase${i}`);
+        el.find('.accordion-collapse.collapse.show').attr('id', `collapse${i}`);
+        el.find('#sitting-times-header-button').attr('aria-controls', `collpase${i}`);
         while (interval < endTime.setMinutes(endTime.getMinutes() - s.resDuration)) {
 
-            let btn = $(`<button type="button" data-sitting-time="${interval}"> ${interval} </button>`);
+            let btn = $(`<button type="button" class="btn btn-outline-primary m-1" data-sitting-time="${interval}"> ${interval} </button>`);
             btn.click((e) => selectSittingInterval($(e.target).data('sitting-time')));
             el.find('#sitting-times-accordian-body').append(btn);
             interval.setMinutes(interval.getMinutes() + 15)
--- ReservationSystemUITests/AddSittingPageUnitTest.cs
+++ ReservationSystemUITests/AddSittingPageUnitTest.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace ReservationSystemUITests
 {
@@ -15,48 +16,104 @@ namespace ReservationSystemUITests
         public void ChromeTitleTest()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(_appUrl);
-            driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
-            driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
-            Assert.IsTrue(driver.Title.Contains("Add Sitting"));
-            driver.Quit();
+            try
+            {
+                driver.Navigate().GoToUrl(_appUrl);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
+                driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
+                Thread.Sleep(500);
+                Assert.IsTrue(driver.Title.Contains("Add Sitting"));
+            }
+            finally
+            {
+                driver.Quit();
+            }
         }
 
         [TestMethod]
         public void ChromeElementsTest()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(_appUrl);
-            driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
-            driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
-            Assert.IsNotNull(driver.FindElement(By.TagName("h1")));
-            Assert.IsNotNull(driver.FindElement(By.TagName("form")));
-            Assert.IsNotNull(driver.FindElement(By.TagName("header")));
-            Assert.IsNotNull(driver.FindElement(By.TagName("footer")));
+            try
+            {
+                driver.Navigate().GoToUrl(_appUrl);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
+                driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
+                Thread.Sleep(500);
+                Assert.IsNotNull(driver.FindElement(By.TagName("h1")));
+                Assert.IsNotNull(driver.FindElement(By.TagName("form")));
+                Assert.IsNotNull(driver.FindElement(By.TagName("header")));
+                Assert.IsNotNull(driver.FindElement(By.TagName("footer")));
+            }
+            finally
+            {
+                driver.Quit();
+            }
+            
         }
 
         [TestMethod]
         public void ChromeNavCountTest()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(_appUrl);
-            driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
-            driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
-            ReadOnlyCollection<IWebElement> navItems = driver.FindElements(By.ClassName("form-group"));
-            Assert.IsTrue(navItems.Count >= 6, "At least 6 navigation items are expected");
-            driver.Quit();
+            try
+            {
+                driver.Navigate().GoToUrl(_appUrl);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
+                driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
+                Thread.Sleep(500);
+                ReadOnlyCollection<IWebElement> navItems = driver.FindElements(By.ClassName("form-group"));
+                Assert.IsTrue(navItems.Count >= 6, "At least 6 navigation items are expected");
+            }finally
+            {
+                driver.Quit();
+            }
+          
+            
         }
         
         [TestMethod]
         public void ChromeTuteLink()
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl(_appUrl);
-            driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
-            driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
-            driver.FindElement(By.Id("admin")).Click();
-            Assert.AreEqual("https://localhost:7156/Admin", driver.Url);
-            driver.Quit();
+            try
+            {
+                driver.Navigate().GoToUrl(_appUrl);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
+                driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("admin")).Click();
+                Assert.AreEqual("https://localhost:7156/Admin", driver.Url);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+
+        [TestMethod]
+        public void ChromeAddSittingFormValidation()
+        {
+            IWebDriver driver = new ChromeDriver();
+            try
+            {
+                driver.Navigate().GoToUrl(_appUrl);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("email")).SendKeys("manager@manager.com");
+                driver.FindElement(By.Id("password")).SendKeys("manager" + Keys.Enter);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("submit")).Click();
+                bool formVal = driver.FindElement(By.TagName("span")).Displayed;
+                Assert.IsTrue(formVal);
+            }
+            finally
+            {
+                driver.Quit();
+            }
         }
 
     }
