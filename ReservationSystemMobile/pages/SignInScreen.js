56321f2146998af3e94b7af50b401528258dfed1
andrewjgardner <knowingandy@gmail.com>
2022-06-07 11:21:05 +1000

    Styling

--- ReservationSystemMobile/pages/SignInScreen.js
+++ ReservationSystemMobile/pages/SignInScreen.js
@@ -1,5 +1,13 @@
 import { useState, useContext } from 'react'
-import { View, Text, Button, TextInput, Pressable, StyleSheet } from 'react-native'
+import {
+    View,
+    Text,
+    Button,
+    TextInput,
+    Pressable,
+    StyleSheet,
+    Image,
+} from 'react-native'
 import { AuthContext } from '../App'
 
 export function SignInScreen({ navigation }) {
@@ -19,22 +27,33 @@ export function SignInScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <Text>Email</Text>
-            <TextInput
-                value={email}
-                onChangeText={setEmail}
-                keyboardType="email-address"
-                style={styles.input}
-            />
-            <Text>Password</Text>
-            <TextInput
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
-                style={styles.input}
-            />
-            <Button title="Sign In" onPress={() => handleSignIn()} />
-            <Text>{error}</Text>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    source={require('../assets/bean-scene-logo.png')}
+                />
+                <Text style={styles.headerText}>Bean Scene</Text>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    value={email}
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    style={styles.input}
+                    placeholder="Email"
+                />
+                <TextInput
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry={true}
+                    style={styles.input}
+                    placeholder="Password"
+                />
+                <Pressable onPress={handleSignIn} style={styles.button}>
+                    <Text style={styles.buttonText}>Sign In</Text>
+                </Pressable>
+                <Text style={styles.errorText}>{error}</Text>
+            </View>
         </View>
     )
 }
@@ -51,6 +70,45 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
         width: 200,
-        backgroundColor: '#d3d1d1'
+        backgroundColor: '#d3d1d1',
     },
-}) 
+    headerText: {
+        color: '#0d6efd',
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    logo: {
+        height: 100,
+        width: 200,
+        resizeMode: 'contain',
+        marginBottom: 20,
+    },
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logoContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+        backgroundColor: '#0d6efd',
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 16,
+    },
+})
