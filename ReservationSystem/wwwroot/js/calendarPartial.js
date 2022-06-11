ad028ed5654526837b6130b8972580ad926b7d5a
deadene <knowingandy@gmail.com>
2022-06-11 11:20:19 +1000

    Calendar updates

--- ReservationSystem/wwwroot/js/calendarPartial.js
+++ ReservationSystem/wwwroot/js/calendarPartial.js
@@ -1,7 +1,11 @@
 ﻿
-$(function () {
+$(async function () {
     const currentDate = new Date();
-    GetSittings(currentDate);  
+    sittings = await getSittingsOnMonth(currentDate);
+    $('.calendar-container').updateCalendarOptions({
+        date: currentDate,
+        disable: disableDates
+    });
 });
 
 async function getSittingsOnMonth(date) {
@@ -21,31 +25,35 @@ function getSittingsOnDay(selectedDate, sittings) {
 
 async function GetSittings(date) {
     $("#sittings-partial").html("");
-    const sittings = await getSittingsOnMonth(date);
+    const sittings = JSON.parse(sessionStorage.getItem('sittings'));
     let s = getSittingsOnDay(date, sittings);
     if (s.length == 0) {
         $("#sittings-partial").html("");
     }
     let i = 0;
-    for (let s of getSittingsOnDay(date,sittings)) {
+    for (let s of getSittingsOnDay(date, sittings)) {
         i++;
         let interval = new Date(s.startTime);
         let endTime = new Date(s.endTime)
 
         let el = $($('#accordionExample').html());
-        el.find('#sitting-times-header-button').html(`${s.title} ${s.startTime} ${s.endTime}`);
+        el.find('#sitting-times-header-button').html(`${s.title} `);
         el.find('#sitting-times-header-button').attr('data-bs-target', `#collpase${i}`);
         el.find('#sitting-times-header-button').attr('aria-controls', `#collpase${i}`);
-        el.find('.accordion-collapse.collapse.show').attr('id', `collpase${i}`);
+        el.find('.accordion-collapse.collapse').attr('id', `collpase${i}`);
         while (interval < endTime.setMinutes(endTime.getMinutes() - s.resDuration)) {
 
-            let btn = $(`<button type="button" class="btn btn-outline-primary m-1" data-sitting-time="${interval}" data-sitting-id="${s.id}"> ${interval} </button>`);
-            btn.click((e) => selectSittingInterval($(e.target).data('sitting-time'),$(e.target).data('sitting-id')));
+            let btn = $(`<button type="button" class="btn btn-outline-primary m-1 time-slot" data-sitting-time="${interval}" data-sitting-id="${s.id}"> ${interval.toLocaleTimeString('en-AU', { hour: 'numeric', minute: 'numeric', hour12: true })} </button>`);
+            btn.click((e) => {
+                $('.time-slot').removeClass('selected-time-slot');
+                $(e.target).addClass('selected-time-slot');
+                selectSittingInterval($(e.target).data('sitting-time'), $(e.target).data('sitting-id'));
+            });
             el.find('#sitting-times-accordian-body').append(btn);
             interval.setMinutes(interval.getMinutes() + 15)
         }
-        $("#sittings-partial").append(el); 
-    } 
+        $("#sittings-partial").append(el);
+    }
 }
 
 function selectSittingInterval(date, id) {
@@ -58,16 +66,31 @@ function selectSittingInterval(date, id) {
 }
 
 function selectDate(date) {
+    let d = new Date(date);
     $('.calendar-container').updateCalendarOptions({
         date: date
     });
+    $('.calendar-container').hide();
+    $('#calendar-show').show();
+    $('#calendar-show').html(d.toLocaleDateString());
     updateSessionPartial(date);
 }
 
+$("#calendar-show").click(() => {
+    $('#calendar-show').hide();
+    $('.calendar-container').show();
+    $("#sittings-partial").html("");
+})
+
 async function updateSessionPartial(date) {
     var d = new Date(date);
+    d.setHours(14);
+    const sittings = await getSittingsOnMonth(d);
+    $('.calendar-container').updateCalendarOptions({
+        date: date,
+        disable: disableDates
+    });
     GetSittings(d);
-        
 }
 
 const calendar = $('.calendar-container').calendar({
@@ -75,8 +98,21 @@ const calendar = $('.calendar-container').calendar({
     date: new Date(),
     onClickDate: selectDate,
     onChangeMonth: updateSessionPartial,
-    showYearDropdown: true,
+    onClickMonthNext: updateSessionPartial,
+    onClickMonthPrev: updateSessionPartial,
     startOnMonday: false,
     prevButton: "<",
     nextButton: ">",
+    disable: disableDates,
 });
+
+function disableDates(date) {
+    const sittings = JSON.parse(sessionStorage.getItem('sittings'));
+    for (var i = 0; i < sittings.length; i++) {
+        let day = new Date(sittings[i].startTime);
+        if (day.getDate() == date.getDate()) {
+            return false;
+        }
+    }
+    return true;
+}
